2a3df8b6565bb770cf5727ee990e00190c79ff5d
quybnz1 <ngotrongquan116@gmail.com>
2021-05-29 09:45:55 +0700

    fix

--- src/main/java/org/teamGame/sounds/SoundLoader.java
+++ src/main/java/org/teamGame/sounds/SoundLoader.java
@@ -3,11 +3,14 @@ package org.teamGame.sounds;
 import javafx.scene.media.Media;
 
 import java.io.File;
+import java.net.URL;
 
 public class SoundLoader {
 
     public static Media loadSound(String path) {
-        return new Media(new File(path).toURI().toString());
+
+        URL ostFile = SoundLoader.class.getResource(path);
+        return new Media(ostFile.toString());
     }
 
 }
